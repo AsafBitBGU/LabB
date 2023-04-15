51553a19e4868df2ab6983eb768cc8d2d006ebc0
AsafBitBGU <118049273+AsafBitBGU@users.noreply.github.com>
2023-04-15 19:01:24 +0300

    Add files via upload

--- LabB/part1/AntiVirus.c
+++ LabB/part1/AntiVirus.c
@@ -35,10 +35,10 @@ virus* readVirus(FILE* file){
     v->sig = malloc(v->SigSize);
     size_t maybeEnd = fread(v->sig, sizeof(char), v->SigSize, file);
     if (maybeEnd == 0){
+        free(v->sig);
         free(v);
         return NULL;
     }
-
     return v;
 }
 
@@ -50,8 +50,6 @@ void printVirus(virus* virus, FILE* output){
         fprintf(output, "%02X ", (unsigned char) virus->sig[i]);
     }
     fprintf(output, "\n");
-    // free(virus->sig);
-    // free(virus);
 }
 
 void list_print(link *virus_list, FILE* output){
@@ -80,6 +78,8 @@ void list_free(link *virus_list){
         if (virus_list->nextVirus != NULL){
             list_free(virus_list->nextVirus);
         }
+        free(virus_list->vir->sig);
+        free(virus_list->vir);
         free(virus_list);
     }
 }
@@ -87,24 +87,21 @@ void list_free(link *virus_list){
 void load_signatures(){
     char str[256];
     char vir[4];
-    //char buff[20];
     printf("%s", "Enter a file name: ");
     fgets(str, 256, stdin);
     str[strcspn(str, "\r\n")] = '\0';
-    // sscanf(buff, "%s",str);
     FILE *file = fopen(str, "r");
     if (file == NULL){
         printf("%s", "No such file!\n");
         exit(1);
     }
     fread(vir, sizeof(vir), 1, file);
-    printf("%s\n", vir);
-    //printf("%02X", (char) vir);
+    printf("%s\n", vir); // need to delete after we handle our situation with the magic number
     if ((strcmp(vir,"VISBsignatures-B") != 0) & (strcmp(vir,"VISLsignatures-L") != 0)){ ///?!
         printf("%s", "Incorrect magic number!!!!\n");
         exit(0);
     }
-    virus *v = malloc(sizeof(virus));
+    virus *v;
     list = malloc(sizeof(link));
     list->vir = readVirus(file);
     list->nextVirus = NULL;
@@ -112,7 +109,6 @@ void load_signatures(){
     {
         list = list_append(list, v);
     }
-    //free(v);
     fclose(file);
 }
 
@@ -124,17 +120,14 @@ void print_signatures(){
 
 void detect_virus(char *buffer, unsigned int size, link *virus_list) {
     int i;
-   // while (virus_list != NULL) {
-        for (i = 0; i < size; i++) {
-            if (memcmp(buffer + i, virus_list->vir->sig, virus_list->vir->SigSize) == 0) {
-                printf("Virus found starting at byte location 0x%02X\n", i);
-                printf("Virus name: %s\n", virus_list->vir->virusName);
-                printf("Virus signature size: %d\n", virus_list->vir->SigSize);
-                printf("\n");
-            }
+    for (i = 0; i < size; i++) {
+        if (memcmp(buffer + i, virus_list->vir->sig, virus_list->vir->SigSize) == 0) {
+            printf("Virus found starting at byte location 0x%02X\n", i);
+            printf("Virus name: %s\n", virus_list->vir->virusName);
+            printf("Virus signature size: %d\n", virus_list->vir->SigSize);
+            printf("\n");
         }
-        //virus_list = virus_list->nextVirus;
-//    }
+    }
 }
 
 void detect_viruses(){
@@ -204,11 +197,9 @@ void quit(){
 struct fun_desc menu[] = {{"Load signatures", load_signatures}, {"Print signatures", print_signatures}, {"Detect viruses", detect_viruses}, {"Fix file", fix_file}, {"Quit", quit}, {NULL,NULL}};
 
 int main(int argc, char **argv){
-    //FILE *out = fopen(argv[2], "w");
     int menu_size = sizeof(menu)/sizeof(menu[0])-1; // Exclude the NULL
     char buffer[menu_size];
     char choice;
-    // char* carray = (char*) malloc(menu_size);
     while(1){
       fprintf(stdout, "%s", "Select operation from the following menu:\n");
       for(int i = 0; i < menu_size; i++){
@@ -219,45 +210,13 @@ int main(int argc, char **argv){
       if (choice == EOF){
         break;
       }
-      //choice = input[0];
 
       if ((choice >= '0') & (choice < (menu_size + '0'))){
-            printf("%s\n", "Within bounds");
             menu[choice-48].fun();
-            // char* temp = carray;
-            // carray = map(carray, menu_size, menu[choice-48].fun);
-            // free(temp);
         } else {
             printf("%s\n", "Not within bounds");
             break;
         }
     }
-    //free(carray);
-
-    // char vir[4];
-    // if (argc < 2){
-    //     printf("%s", "Not Enogh Arguments!\n");
-    //     exit(0);
-    // }
-    // FILE * file = fopen(argv[1], "r");
-    // if (file == NULL){
-    //     printf("%s", "Failed to open output file!\n");
-    //     exit(0);
-    // }
-    // fread(vir, sizeof(vir), 1, file);
-    // printf("%s\n", vir);
-    // //printf("%02X", (char) vir);
-    // if ((strcmp(vir,"VISB") != 0) & (strcmp(vir,"VISL") != 0)){
-    //     printf("%s", "Incorrect magic number!!!!\n");
-    //     exit(0);
-    // }
-    // virus *v;
-    // while ((v = readVirus(file)) != NULL)
-    // {
-    //     //v = readVirus(file);
-    //     printVirus(v, stdout);
-    // }
-    // fclose(file);
-    //fclose(out);
     return 0;
 }
